420ad5963425d5d3b3cb0eb3c2e51df362d14e28
arman455 <snositel99@gmail.com>
2024-11-26 22:42:40 +0200

    Amount of post created

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,3 +1,6 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
+
 interface PostProps {
     header: string;
     description: string;
@@ -7,12 +10,25 @@ interface PostProps {
 }
 
 export function Post(props: PostProps){
+
+    const [amount, setAmount] = useState(0)
+    const [isLiked, setIsLiked] = useState(false);
+
+    function Like() {
+        if (!isLiked) {
+            setAmount(amount + 1);
+            setIsLiked(true);
+        }
+    }
+
     return (
         <div>
             <h1>{props.header}</h1>
             <p>{props.description}</p>
             <img width={300} height={300} src={props.image} alt="imagePost" />
             <p>{props.author}</p>
+            <p>{amount}</p>
+            <button onClick={Like}>Like</button>
         </div>
     )
 }
