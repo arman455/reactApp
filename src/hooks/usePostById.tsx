be68ead654cfefe8a6ecd5959e8f470bd256820f
arman455 <snositel99@gmail.com>
2024-12-21 19:24:14 +0200

    added loading

--- src/hooks/usePostById.tsx
+++ src/hooks/usePostById.tsx
@@ -4,16 +4,18 @@ import { IPost } from "./usePosts"
 export function usePostsById(id: number){
 
     const [post, setPost] = useState<IPost>()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         async function getPost(){
             const response = await fetch(`https://dev.to/api/articles/${id}`)
             const post = await response.json()
             setPost(post)
+            setIsLoading(false)
         }
         getPost()
     },[id])
 
-    return {post: post}
+    return {post: post, isLoading: isLoading}
 
 }
--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,21 +1,34 @@
 import { Link, useParams } from "react-router-dom"
 import { usePostsById } from "../../hooks/usePostById"
 import "./PostPage.css"
+import { Audio } from 'react-loader-spinner';
+<Audio
+  height="80"
+  width="80"
+  color="green"
+  ariaLabel="three-dots-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+/>
 
 export function PostPage(){
 
     const params = useParams();
-    const { post }= usePostsById(Number(params.id))
+    const { post, isLoading }= usePostsById(Number(params.id))
 
     return (
         <div className="postpage">
-            <h1 className="postpagemaintext">{post?.title}</h1>
-            <img className="postpageimg" src={post?.social_image} alt="" />
-            <p className="postpagetext">Tags: {post?.tags.join(", ")}</p>
-            <p className="postpagetext">Description: {post?.description}</p>
-            <Link to={"/posts"}>
-                <button className="buttondetailed">Back</button>
-            </Link>
+            { isLoading === true ? (<Audio height="80" width="80" color="grey" ariaLabel="three-dots-loading" wrapperStyle={{}} wrapperClass=""/>) : (
+                <>
+                    <h1 className="postpagemaintext">{post?.title}</h1>
+                    <img className="postpageimg" src={post?.social_image} alt="" />
+                    <p className="postpagetext">Tags: {post?.tags.join(", ")}</p>
+                    <p className="postpagetext">Description: {post?.description}</p>
+                    <Link to={"/posts"}>
+                        <button className="buttondetailed">Back</button>
+                    </Link>
+                </>
+            )}
         </div>
     )
 }
